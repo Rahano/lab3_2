605faa31320e7a1d8072335414099bb250e99355
Muras.Wojciech <215828@edu.p.lodz.pl>
2021-01-09 22:49:25 +0100

    Implement fake clock

--- src/main/java/edu/iis/mto/time/Order.java
+++ src/main/java/edu/iis/mto/time/Order.java
@@ -1,6 +1,10 @@
 package edu.iis.mto.time;
 
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.joda.time.DateTime;
@@ -10,10 +14,15 @@ public class Order {
 
     private static final int VALID_PERIOD_HOURS = 24;
     private State orderState;
-    private List<OrderItem> items = new ArrayList<OrderItem>();
-    private DateTime subbmitionDate;
+    private List<OrderItem> items = new ArrayList<>();
+    private Instant subbmitionDate;
+    private Clock clock;
 
     public Order() {
+        orderState = State.CREATED;
+    }
+    public Order(Clock clock){
+        this.clock = clock;
         orderState = State.CREATED;
     }
 
@@ -29,14 +38,14 @@ public class Order {
         requireState(State.CREATED);
 
         orderState = State.SUBMITTED;
-        subbmitionDate = new DateTime();
+        subbmitionDate = clock.instant();
 
     }
 
     public void confirm() {
         requireState(State.SUBMITTED);
-        int hoursElapsedAfterSubmittion = Hours.hoursBetween(subbmitionDate, new DateTime())
-                                               .getHours();
+        int hoursElapsedAfterSubmittion = (int) Duration.between(subbmitionDate, clock.instant())
+                                                        .toHours();
         if (hoursElapsedAfterSubmittion > VALID_PERIOD_HOURS) {
             orderState = State.CANCELLED;
             throw new OrderExpiredException();
@@ -60,7 +69,7 @@ public class Order {
         }
 
         throw new OrderStateException("order should be in state "
-                                      + allowedStates
+                                      + Arrays.toString(allowedStates)
                                       + " to perform required  operation, but is in "
                                       + orderState);
 
