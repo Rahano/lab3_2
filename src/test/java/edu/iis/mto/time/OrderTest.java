debe586c27f445d52003d18a9a4fa7cdb04eda87
Muras.Wojciech <215828@edu.p.lodz.pl>
2021-01-09 23:46:57 +0100

    Implement tests for Order class

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,9 +1,12 @@
 package edu.iis.mto.time;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.time.Clock;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +18,26 @@ class OrderTest {
     public void setUp(){
         mockedClock = Mockito.mock(Clock.class);
         order = new Order(mockedClock);
+    }
+
+    @Test
+    public void orderNotExpiredImmediately(){
+        Mockito.when(mockedClock.instant()).thenReturn(Instant.now());
+        order.submit();
+        order.confirm();
+    }
+
+    @Test
+    public void orderNotExpiredWithValidTime(){
+        Mockito.when(mockedClock.instant()).thenReturn(Instant.now(), Instant.now().plus(24, ChronoUnit.HOURS));
+        order.submit();
+        order.confirm();
+    }
+    @Test
+    public void OrderExpiredWithValidDate(){
+        Mockito.when(mockedClock.instant()).thenReturn(Instant.now(), Instant.now().plus(25, ChronoUnit.HOURS));
+        order.submit();
+        assertThrows(OrderExpiredException.class, order::confirm);
     }
 
 }
